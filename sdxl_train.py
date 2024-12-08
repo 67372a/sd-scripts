7961cec92a6b605db61a89779ca1703e2d975c42
67372a <117533205+67372a@users.noreply.github.com>
2024-12-07 19:22:48 -0600

    sdxl_train args
    Remove duplicate edm2_loss_weighting
    Add immiscible_noise and immiscible_diffusion args

--- sdxl_train.py
+++ sdxl_train.py
@@ -1321,12 +1321,6 @@ def setup_parser() -> argparse.ArgumentParser:
     )
 
     parser.add_argument(
-        "--edm2_loss_weighting",
-        action="store_true",
-        help="Use EDM2 loss weighting.",
-    )
-
-    parser.add_argument(
         "--loss_multipler",
         type=float,
         default=None,
@@ -1444,6 +1438,20 @@ def setup_parser() -> argparse.ArgumentParser:
         default=1.0,
         help="A scaling factor to apply to the decay rate of the edm2_loss_weighting_lr_scheduler, lower values result in slower decay, higher values result in faster decay.",
     )
+
+    parser.add_argument(
+            "--immiscible_noise",
+            type=int,
+            default=None,
+            help="Batch size to match noise to latent images. Use Immiscible Noise algorithm to project training images only to nearby noise (from arxiv.org/abs/2406.12303) "
+            + "/ ノイズを潜在画像に一致させるためのバッチ サイズ。Immiscible Noise ノイズアルゴリズを使用して、トレーニング画像を近くのノイズにのみ投影します（arxiv.org/abs/2406.12303 より）",
+        )
+    
+    parser.add_argument(
+            "--immiscible_diffusion",
+            action="store_true",
+            help="Use immiscible diffusion to generate noised latents instead of standard noise scheduler. Mutually exclusive with ip noise gamma.",
+        )
 
     return parser
 
