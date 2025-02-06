a94ff23451f2b453d61345eac4194f62f01bb8c2
67372a <117533205+67372a@users.noreply.github.com>
2025-02-06 11:40:27 -0600

    detach and set norm type explictly

--- train_network.py
+++ train_network.py
@@ -71,9 +71,9 @@ def analyze_gradient_norms(parameters):
     
     for param in parameters:
         if param.grad is not None:
-            grad = param.grad
+            grad = param.grad.detach()
 
-            norm = torch.norm(grad).item()
+            norm = torch.norm(grad, p=2.0).item()
             if not (np.isnan(norm) or np.isinf(norm)):
                 grad_norms.append(norm)
     
