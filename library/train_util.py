120d683cfc9e2d5940f5a005ce544589f6c65c17
67372a <117533205+67372a@users.noreply.github.com>
2025-02-15 11:49:21 -0600

    Fix to allow schedulers with prodigyplus

--- library/train_util.py
+++ library/train_util.py
@@ -5328,7 +5328,7 @@ def get_scheduler_fix(args, optimizer: Optimizer, num_processes: int):
     Unified API to get any scheduler from its name.
     """
     # if schedulefree optimizer, return dummy scheduler
-    if args.optimizer_type.lower().split(".")[0] not in {"LoraEasyCustomOptimizer".lower()} and is_schedulefree_optimizer(optimizer, args):
+    if args.optimizer_type.lower().split(".")[0] not in {"LoraEasyCustomOptimizer".lower(), "prodigyplus".lower()} and is_schedulefree_optimizer(optimizer, args):
         return get_dummy_scheduler(optimizer)
     
     # Need to apply scheduler to base_optimizer
